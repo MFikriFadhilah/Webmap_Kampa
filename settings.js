ebf4ee72d52abf3542e33ecb2449dd44a4991d78
MFikriFadhilah <90301397+MFikriFadhilah@users.noreply.github.com>
2021-11-19 15:37:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 // Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
 // or a Google Sheets URL (File > Publish to the Web > CSV )
-const dataLocation = 'data/places.csv';
+const dataLocation = 'data/LPHA_Kampa.csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [0.6795, 101.345];
+const mapCenter = [0.329401, 1001.214294];
 const mapZoom = 12;
 
 // Marker icon height and width
@@ -11,5 +11,5 @@ const iconHeight = 30;
 const iconWidth = 30;
 
 //Icon for pohon muda and pohon dewasa
-const pohonDewasa =  'logo/pohon_dewasa.svg'
-const pohonMuda = 'logo/tree-5202.svg'
+const pohonDewasa =  'logo/mature_tree.svg'
+const pohonMuda = 'logo/young_tree.svg'
